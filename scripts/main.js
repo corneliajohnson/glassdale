5f29d68c0a55b148f38992750529ec237d478708
Cornelia Johnson <corneliaj91@gmail.com>
2020-08-31 14:10:52 -0400

    css changes

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,4 @@ import { CriminalList } from "./criminals/CrminalList.js";
 import { OfficerList } from "./officers/OfficerList.js";
 
 CriminalList();
-OfficerList();
+//OfficerList();
--- scripts/officers/Officers.js
+++ scripts/officers/Officers.js
@@ -1,6 +1,6 @@
 export const officersHTML = (officerObj) => {
   return `
-  <section id="officer=${officerObj.id}" class="officer">
+  <section id="officer=${officerObj.id}" class="card-officer">
     <h2>${officerObj.name}</h2>
     </section>
   `;
