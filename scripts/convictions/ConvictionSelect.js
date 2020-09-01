43b23bd7139f4c00a64e946ecceea8f40bf29647
Cornelia Johnson <corneliaj91@gmail.com>
2020-09-01 15:14:22 -0400

    added event hub abd rendered to page

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -1,14 +1,25 @@
 import { useConvictions, getConvictions } from "./ConvictionProvider.js";
 
 // Get a reference to the DOM element where the <select> will be rendered
+const eventHub = document.querySelector(".container");
 const contentTarget = document.querySelector(".filters__crime");
 
-export const ConvictionSelect = () => {
-  getConvictions().then(() => {
-    const convictions = useConvictions();
-    render(convictions);
-  });
-};
+// On the event hub, listen for a "change" event.
+eventHub.addEventListener("change", (event) => {
+  // Only do this if the `crimeSelect` element was changed
+  if (event.target.id === "crimeSelect") {
+    // Create custom event. Provide an appropriate name.
+    const customEvent = new CustomEvent("crimeChosen", {
+      detail: {
+        crimeThatWasChosen: event.target.value,
+        crimeId: event.target.id,
+      },
+    });
+
+    // Dispatch to event hub
+    eventHub.dispatchEvent(customEvent);
+  }
+});
 
 const render = (convictionsCollection) => {
   contentTarget.innerHTML = `
@@ -17,9 +28,16 @@ const render = (convictionsCollection) => {
     ${convictionsCollection
       .map(
         (conviction) =>
-          `<option value=${conviction.name}>${conviction.name}</option>`
+          `<option id="crimeId" value="${conviction.name}">${conviction.name}</option>`
       )
       .sort()}
   </select>   
     `;
+};
+
+export const ConvictionSelect = () => {
+  getConvictions().then(() => {
+    const convictions = useConvictions();
+    render(convictions);
+  });
 };
--- scripts/criminals/CrminalList.js
+++ scripts/criminals/CrminalList.js
@@ -1,16 +1,37 @@
 import { getCriminals, useCriminals } from "./CriminalProvider.js";
 import { criminalListHTML } from "./Criminal.js";
+const eventHub = document.querySelector(".container");
+let criminalArray = [];
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener("crimeChosen", (event) => {
+  // You remembered to add the id of the crime to the event detail, right?
+  if ("crimeId" in event.detail) {
+    /*
+          Filter the criminals application state down to the people that committed the crime
+      */
+    const matchingCriminals = criminalArray.filter((matchingCriminal) => {
+      return matchingCriminal.conviction === event.detail.crimeThatWasChosen;
+    });
 
-export const CriminalList = () => {
-  getCriminals().then(() => {
-    const criminalArray = useCriminals();
-    addOfficerToDOM(criminalArray);
-  });
+    /*
+          Then invoke render() and pass the filtered collection as
+          an argument
+      */
+    render(matchingCriminals);
+  }
+});
+
+const render = (theCriminalArray) => {
+  const criminalsContainer = document.querySelector(".criminalsContainer");
+  criminalsContainer.innerHTML = `
+    ${theCriminalArray.map((criminal) => criminalListHTML(criminal)).join("")}
+  `;
 };
 
-const addOfficerToDOM = (theOfficerArray) => {
-  const criminalsContainer = document.querySelector(".criminalsContainer");
-  return theOfficerArray.map((criminal) => {
-    criminalsContainer.innerHTML += criminalListHTML(criminal);
+// Render ALL criminals initally
+export const CriminalList = () => {
+  getCriminals().then(() => {
+    criminalArray = useCriminals();
+    render(criminalArray);
   });
 };
